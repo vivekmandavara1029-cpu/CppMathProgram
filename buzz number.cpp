512c0e92192c2711dcd9ea271f6f3b0157f7cc76
vivek mandavara <vivekmandavara1029@gmail.com>
2026-08-17 21:04:27 +0530

    upload buzz number file and check it buzz or not ?
    the best describes the buzz number in README.md
    example of buzz number is 107 ,77, 117 etc.

---
+++ buzz number.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a;
+	cout<<"enter the value of n ";
+	cin>>a;
+	if(a%7==0)
+	cout<<"it is a buzz number";
+else if((a+3)%10==0)
+	cout<<"it is a buzz number";
+	else
+	cout<<"it is not a buzz number";
+	return 0;}
