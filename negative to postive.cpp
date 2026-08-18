48df8fcfd8db00f561ee44e1a0e7f2d942e71825
vivek mandavara <vivekmandavara1029@gmail.com>
2026-08-18 21:36:10 +0530

    print the number from negative to positive.
    for example :
    the given input is 5 
    now, 
    the output is print the series (-5 to 5 ) like  -5 , -4 , -3 ............5

---
+++ negative to postive.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std; 
+int main(){
+	int a, i,n;
+	cout<<"enter no";
+	cin>>n;
+	for(i=-n;i<0;i++){
+	if(i%2!=0)
+	cout<<i<<endl;}
+	for(a=1;a<=n;a++){
+	if(a%2==0)
+	cout<<a<<endl;}
+	return 0;}
+	
