86ea1c7b30c5a02bc303ec2e46be312a76110f6f
vivek mandavara <vivekmandavara1029@gmail.com>
2026-08-25 17:39:29 +0530

    find the Area ,Circumference and Diameter of a circle.
    for example : if user given input (radius) = 5
    now, the output is-
    area , circumference and diameter.

---
+++ find about circle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+int circumference,area,diameter,radius;
+cout <<"enter the value of radius = ";
+cin >>radius;
+circumference= 2*3.14*radius;
+area=3.14*radius*radius;
+diameter= 2*radius;
+cout<<"the circumference of a circle is  "<<circumference<<endl;
+cout<<"the area of a circle is "<<area<< endl;
+cout << "the diameter of a circle is "<<diameter;
+return 0;}
+
